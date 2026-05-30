763573d62faa0578ceb7d03ee8eb7bf270d026db
Shreesh <shreeshtripathi3@gmail.com>
2026-05-30 13:37:53 +0530

    add 8

---
+++ 08_calculator.py
@@ -0,0 +1,25 @@
+operator = input("Enter an operator (+, -, *, /): ")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(f"The result of {num1} + {num2} is: {round(result, 2)}")
+
+elif operator == "-":
+    result = num1 - num2
+    print(f"The result of {num1} - {num2} is: {round(result, 2)}")
+
+elif operator == "*":
+    result = num1 * num2
+    print(f"The result of {num1} * {num2} is: {round(result, 2)}")
+
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print(f"The result of {num1} / {num2} is: {round(result, 2)}")
+    else:
+        print("Error: Division by zero is not allowed.")
+
+else: 
+    print("Invalid operator. Please enter one of the following: +, -, *, /.")
