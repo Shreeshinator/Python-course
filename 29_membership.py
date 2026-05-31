9a577802bce4f1ab5720a8b6661cb89e0825336b
Shreeshinator <shreeshtripathi13@gmail.com>
2026-05-31 08:51:22 +0530

    update

--- 29_membership.py
+++ 29_membership.py
@@ -13,3 +13,10 @@ if letter not in word:
     print(f"{letter} is not in the word {word}.")
 else:
     print(f"{letter} is in the word {word}.")
+
+#email demo 
+email = "hello123@gmail.com"
+if "@" in email and "." in email:
+    print(f"{email} is a valid email address.")
+else:
+    print(f"{email} is not a valid email address.")
