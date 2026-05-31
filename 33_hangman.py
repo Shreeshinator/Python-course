d8c1264e04ac49df82678f364126a5a8b1c48ac7
Shreeshinator <shreeshtripathi13@gmail.com>
2026-05-31 15:39:46 +0530

    add hangman

---
+++ 33_hangman.py
@@ -0,0 +1,81 @@
+import random
+
+hangman_art = {0: ("   ",
+                                   "   ",
+                                   "   "),
+                             1: (" o ",
+                                   "   ",
+                                   "   "),
+                             2: (" o ",
+                                   " | ",
+                                   "   "),
+                             3: (" o ",
+                                   "/| ",
+                                   "   "),
+                             4: (" o ",
+                                  "/|\\",
+                                   "   "),
+                              5: (" o ",
+                                   "/|\\",
+                                   "/  "),
+                              6: (" o ",
+                                   "/|\\",
+                                   "/ \\")}
+
+words = ("aardvark", "alligator", "alpaca", "ant", "anteater", "antelope", "ape", "armadillo", "baboon", "badger", "bat", "bear", "beaver", "bee", "bison", "boar", "buffalo", "butterfly", "camel", "capybara", "caribou", "cat", "caterpillar", "cattle", "chamois", "cheetah", "chicken", "chimpanzee", "chinchilla", "chough", "clam", "cobra", "cockroach", "cod", "coyote", "crab", "crane", "crocodile", "crow", "curlew", "deer", "dinosaur", "dog", "dogfish", "dolphin", "donkey", "dormouse", "dotterel", "dove", "dragonfly", "duck", "dugong", "dunlin", "eagle", "echidna", "eel", "eland", "elephant",  "elk", "emu", "falcon", "ferret", "finch", "fish", "flamingo", "fly", "fox", "frog", "gaur", "gazelle", "gerbil", "giraffe", "gnat", "gnu", "goat", "goldfinch", "goldfish", "goose", "gorilla", "goshawk", "grasshopper", "grouse", "guanaco", "gull", "hamster", "hare", "hawk", "hedgehog", "heron", "herring", "hippopotamus", "hornet", "horse", "human", "hummingbird", "hyena", "ibex", "ibis", "jackal", "jaguar", "jay", "jellyfish", "kangaroo", "kingfisher", "koala", "kookabura", "kouprey", "kudu", "lapwing", "lark", "lemur", "leopard", "lion", "llama", "lobster", "locust", "loris", "louse", "lyrebird", "magpie", "mallard", "manatee", "mandrill", "mantis", "marten", "meerkat", "mink", "mole", "mongoose", "monkey", "moose", "mosquito", "mouse", "mule", "narwhal", "newt", "nightingale", "octopus", "okapi", "opossum", "oryx", "ostrich", "otter", "owl", "ox", "oyster", "panda", "panther", "parrot", "partridge", "peafowl", "pelican", "penguin", "pheasant", "pig", "pigeon", "polar-bear", "pony", "porcupine", "porpoise", "quail", "quelea", "quetzal", "rabbit", "raccoon", "rail", "ram", "rat", "raven", "red-deer", "red-panda", "reindeer", "rhinoceros", "rook", "salamander", "salmon", "sand-dollar", "sandpiper", "sardine", "scorpion", "seahorse", "seal", "shark", "sheep", "shrew", "skunk", "snail", "snake", "sparrow", "spider", "spoonbill", "squid", "squirrel", "starling", "stingray", "stoat", "stork", "swallow", "swan", "tapir", "tarsier", "termite", "tiger", "toad", "trout", "turkey", "turtle", "viper", "vulture", "wallaby", "walrus", "wasp", "weasel", "whale", "wildcat", "wolf", "wolverine", "wombat", "woodcock", "woodpecker", "worm", "wren", "yak", "zebra")
+
+
+def display_hangman(tries):
+    print("******************")
+    for line in hangman_art[tries]:
+        print(line)
+    print("******************")
+
+def display_hint(word, guessed_letters):
+    display = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter + ""
+        else:
+            display += "_" 
+    print(display.strip())
+
+def display_answer(word):
+    print(f"The word was: {word}")
+
+def main():
+    word = random.choice(words)
+    guessed_letters = set()
+    tries = 0
+    max_tries = 6
+
+    print("Welcome to Hangman!")
+    
+    while tries < max_tries:
+        display_hangman(tries)
+        display_hint(word, guessed_letters)
+        
+        guess = input("Guess a letter: ").lower()
+        
+        if guess in guessed_letters:
+            print("You already guessed that letter. Try again.")
+            continue
+        
+        guessed_letters.add(guess)
+        
+        if guess in word:
+            print("Good guess!")
+            if all(letter in guessed_letters for letter in word):
+                print(f"Congratulations! You guessed the word: {word}")
+                break
+        else:
+            print("Wrong guess.")
+            tries += 1
+    
+    if tries == max_tries:
+        display_hangman(tries)
+        print("Game over! You've been hanged.")
+        display_answer(word)
+
+if __name__ == "__main__":
+    main()
